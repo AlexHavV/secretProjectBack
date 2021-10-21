709acad16978098854208bb9aa4c4f492b0b6a74
makentoshP <maksim.efimchuk@gmail.com>
2021-10-21 12:39:50 +0300

    [*] Added Cart model and controller

---
+++ SecretProjectBack/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using SecretProjectBack.Context;
+using SecretProjectBack.Entity.Product;
+using SecretProjectBack.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SecretProjectBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly EFContext _context;
+        private readonly IMapper _mapper;
+
+        public CartController(EFContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("CartGetProduct")]
+        public IActionResult CartGetProduct([FromForm]CartGetProductsModel model)
+        {
+            var query = _context.Products.AsQueryable();
+
+            var temp = query
+                .Where(user => model.ArrayId.Contains(user.Id))
+                .Include(x => x.ProductImages.OrderBy(y => y.Priority))
+                .Select(x => _mapper.Map<ProductViewModel>(x)).ToList();
+
+
+            return Ok(temp);
+        }
+    }
+}
--- SecretProjectBack/Controllers/ProductController.cs
+++ SecretProjectBack/Controllers/ProductController.cs
@@ -206,5 +206,6 @@ namespace SecretProjectBack.Controllers
             }
             
         }
+
     }
 }
