15fd74615c7fade6a0c0d2db73df519f53dc9cc7
makentoshP <maksim.efimchuk@gmail.com>
2021-10-29 11:01:31 +0300

    [*] I do not know what i done here

--- SecretProjectBack/Models/UserModels.cs
+++ SecretProjectBack/Models/UserModels.cs
@@ -17,6 +17,15 @@ namespace SecretProjectBack.Models
         public IFormFile Image { get; set; }
     }
 
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Image { get; set; }
+    }
+
     public class UserAddModel
     {
         public string UserName { get; set; }
